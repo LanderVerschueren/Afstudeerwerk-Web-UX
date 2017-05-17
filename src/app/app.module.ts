19926c0aca894bb3d61de44cd8986f391279c4cf
Lander Verschueren <lander.verschueren@thesedays.com>
2017-05-17 13:03:15 +0200

    component: home, header, footer, searchinput, howto, highlight + styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchinputComponent } from './searchinput/searchinput.component';
+import { HowtoComponent } from './howto/howto.component';
+import { HighlightComponent } from './highlight/highlight.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchinputComponent,
+    HowtoComponent,
+    HighlightComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/searchinput/searchinput.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'searchinput-component',
+  templateUrl: './searchinput.component.html',
+  styleUrls: ['./searchinput.component.scss']
+})
+export class SearchinputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
