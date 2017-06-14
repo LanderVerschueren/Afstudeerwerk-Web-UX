100e3051f3a77e3f691a60c956ce3acb7534fa46
Lander Verschueren <lander.verschueren@thesedays.com>
2017-06-14 17:03:55 +0200

    google analytics

--- src/app/services/general.service.ts
+++ src/app/services/general.service.ts
@@ -27,8 +27,8 @@ export class GeneralService {
 	public filterParam:string = 'all';
 
 	// public apilink:string = "http://10.242.24.160:8888/eindwerk/Afstudeerwerk-Web-UX/back-end/public/api/";
-	public apilink:string = "http://localhost:8888/eindwerk/Afstudeerwerk-Web-UX/back-end/public/api/";
-	// public apilink:string = "https://landerverschueren.webhosting.be/back-end/public/api/";
+	// public apilink:string = "http://localhost:8888/eindwerk/Afstudeerwerk-Web-UX/back-end/public/api/";
+	public apilink:string = "https://landerverschueren.webhosting.be/back-end/public/api/";
 	private googleApiKey:string = "AIzaSyCH7nKkRCoG6ONdK2iBhS_xI1LZSJPkJQs";
 
   	constructor( private apicallService : ApicallService, private http: Http, private zone: NgZone ) {
